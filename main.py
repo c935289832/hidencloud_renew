9ccb208578b14ee87cb44eeaf0434531ee434b41
c935289832 <59242574+c935289832@users.noreply.github.com>
2026-01-22 11:36:27 +0800

    Update main.py

--- main.py
+++ main.py
@@ -143,8 +143,8 @@ class HidenCloudBot:
         self.session.headers.update({
             'Host': 'dash.hidencloud.com',
             'Connection': 'keep-alive',
-            'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/120.0.0.0 Safari/537.36',
-            'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,*/*;q=0.8',
+            'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/140.0.0.0 Safari/537.36',
+            'Accept': '*/*',
             'Referer': 'https://dash.hidencloud.com/',
         })
 
